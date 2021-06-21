84e26060b8a6da31c52dec919816b155837254be
diffumist <misanzhiwu@gmail.com>
2021-06-22 00:01:33 +0800

    Integration of nix modules

--- nixos/hosts/local/system.nix
+++ nixos/hosts/local/system.nix
@@ -25,5 +25,10 @@
   services.pipewire = {
       enable = true;
       pulse.enable = true;
+      alsa = {
+        enable = true;
+        support32Bit = true;
+      };
+      jack.enable = true;
   };
 }
