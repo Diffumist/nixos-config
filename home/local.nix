b31b1c39fa9ced1a2d854a78ee83208ecf7aca2d
diffumist <git@diffumist.me>
2021-07-08 15:49:39 +0800

    add xdg dirs env variables

--- home/local.nix
+++ home/local.nix
@@ -1,4 +1,4 @@
-{ pkgs, config, ... }:
+{ ... }:
 
 {
   programs.home-manager.enable = true;
@@ -21,28 +21,6 @@
   ];
 
   fonts.fontconfig.enable = true;
-
-  systemd.user.sessionVariables = {
-    GOOGLE_DEFAULT_CLIENT_ID = "77185425430.apps.googleusercontent.com";
-    GOOGLE_DEFAULT_CLIENT_SECRET = "OTJgUOQcT7lO7GsGZq2G4IlT";
-    LIBVA_DRIVER_NAME = "iHD";
-    # cache
-    XCOMPOSECACHE = "${config.xdg.cacheHome}/compose";
-    __GL_SHADER_DISK_CACHE_PATH = "${config.xdg.cacheHome}/nv";
-    _JAVA_OPTIONS = "-Djava.util.prefs.userRoot=${config.xdg.cacheHome}/java";
-    MYSQL_HISTFILE = "${config.xdg.cacheHome}/mysql_history";
-    NODE_REPL_HISTORY = "${config.xdg.cacheHome}/node_repl_history";
-    HISTFILE = "${config.xdg.cacheHome}/bash_history";
-    # data
-    WAKATIME_HOME = "${config.xdg.dataHome}/wakatime";
-    WINEPREFIX = "${config.xdg.dataHome}/wineprefixes/default";
-    GTK2_RC_FILES = "${config.xdg.dataHome}/gtk-2.0/gtkrc";
-    CABAL_DIR = "${config.xdg.dataHome}/cabal";
-    CABAL_CONFIG = "${config.xdg.dataHome}/cabal/config";
-    KDEHOME = "${config.xdg.dataHome}/kde";
-    LESSHISTFILE = "${config.xdg.dataHome}/lesshst";
-    CARGO_HOME = "${config.xdg.dataHome}/cargo";
-  };
 
   # This value determines the Home Manager release that your
   # configuration is compatible with. This helps avoid breakage
--- home/modules/user-dirs.nix
+++ home/modules/user-dirs.nix
@@ -1,21 +1,42 @@
 { config, ... }:
+let
+  xdgdirs = {
+    GOOGLE_DEFAULT_CLIENT_ID = "77185425430.apps.googleusercontent.com";
+    GOOGLE_DEFAULT_CLIENT_SECRET = "OTJgUOQcT7lO7GsGZq2G4IlT";
+    LIBVA_DRIVER_NAME = "iHD";
+    # cache
+    XCOMPOSECACHE = "${config.xdg.cacheHome}/compose";
+    __GL_SHADER_DISK_CACHE_PATH = "${config.xdg.cacheHome}/nv";
+    _JAVA_OPTIONS = "-Djava.util.prefs.userRoot=${config.xdg.cacheHome}/java";
+    MYSQL_HISTFILE = "${config.xdg.cacheHome}/mysql_history";
+    NODE_REPL_HISTORY = "${config.xdg.cacheHome}/node_repl_history";
+    HISTFILE = "${config.xdg.cacheHome}/bash_history";
+    # config
+    NPM_CONFIG_USERCONFIG = "${config.xdg.configHome}/npm/npmrc";
+    # data
+    WAKATIME_HOME = "${config.xdg.dataHome}/wakatime";
+    WINEPREFIX = "${config.xdg.dataHome}/wineprefixes/default";
+    GTK2_RC_FILES = "${config.xdg.dataHome}/gtk-2.0/gtkrc";
+    CABAL_DIR = "${config.xdg.dataHome}/cabal";
+    CABAL_CONFIG = "${config.xdg.dataHome}/cabal/config";
+    KDEHOME = "${config.xdg.dataHome}/kde";
+    LESSHISTFILE = "${config.xdg.dataHome}/lesshst";
+    CARGO_HOME = "${config.xdg.dataHome}/cargo";
+  };
+in
 {
   xdg = {
-    userDirs =
-      let
-        prefix = "$HOME/.local/share/xdg";
-      in
-      {
-        enable = true;
-        desktop = "$HOME/Desktop";
-        download = "$HOME/Downloads";
-        pictures = "$HOME/Pictures";
-        documents = "$HOME/Documents";
-        music = "$HOME/Music";
-        publicShare = "$HOME";
-        templates = "$HOME";
-        videos = "$HOME/Videos";
-      };
+    userDirs = {
+      enable = true;
+      desktop = "$HOME/Desktop";
+      download = "$HOME/Downloads";
+      pictures = "$HOME/Pictures";
+      documents = "$HOME/Documents";
+      music = "$HOME/Music";
+      publicShare = "$HOME";
+      templates = "$HOME";
+      videos = "$HOME/Videos";
+    };
     configFile = {
       "go/env".text = ''
         GOPATH=${config.xdg.cacheHome}/go
@@ -24,6 +45,14 @@
         GOPROXY=https://goproxy.cn
         GOSUMDB=sum.golang.google.cn
       '';
+      "npm/npmrc".text = ''
+        prefix=${config.xdg.dataHome}/npm
+        cache=${config.xdg.cacheHome}/npm
+        tmp=$XDG_RUNTIME_DIR/npm
+        init-module=${config.xdg.configHome}/npm/config/npm-init.js
+        store-dir=${config.xdg.dataHome}/pnpm-store
+      '';
     };
   };
+  systemd.user.sessionVariables = xdgdirs;
 }
