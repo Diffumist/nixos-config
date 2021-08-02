d92eb64462944ed0a66030e4ae073f2c5a2a0838
diffumist <git@diffumist.me>
2021-08-02 15:45:09 +0800

    add: clash service

--- nixos/hosts/local/network.nix
+++ nixos/hosts/local/network.nix
@@ -8,6 +8,55 @@
     nameservers = [ "127.0.0.1" ];
   };
 
+  systemd.services.clash =
+    let
+      inherit (pkgs) gnugrep iptables clash iproute2;
+      preStartScript = pkgs.writeShellScript "clash-prestart" ''
+        iptables() {
+          ${iptables}/bin/iptables -w "$@"
+        }
+        ip() {
+          ${iproute2}/bin/ip
+        }
+        iptables -t mangle -F CLASH
+        iptables -t mangle -N CLASH
+        iptables -t mangle -A CLASH -d 0.0.0.0/8 -j RETURN
+        iptables -t mangle -A CLASH -d 10.0.0.0/8 -j RETURN
+        iptables -t mangle -A CLASH -d 192.168.0.0/16 -j RETURN
+        iptables -t mangle -A CLASH -d 127.0.0.0/8 -j RETURN
+        iptables -t mangle -A CLASH -j MARK --set-xmark 129
+        iptables -t mangle -A PREROUTING -p udp -m udp --dport 4096:65535 -j RETURN
+        iptables -t mangle -A PREROUTING -p tcp -m tcp --dport 8192:65535 -j RETURN
+        iptables -t mangle -A PREROUTING -j CLASH
+        iptables -t mangle -A OUTPUT -m owner --uid-owner clash -j RETURN
+        iptables -t mangle -A OUTPUT -j CLASH
+      '';
+
+      postStopScript = pkgs.writeShellScript "clash-poststop" ''
+        ${iptables}/bin/iptables-save -c|${gnugrep}/bin/grep -v CLASH|${iptables}/bin/iptables-restore -c
+      '';
+    in
+    {
+      after = [ "network.target" ];
+      wantedBy = [ "multi-user.target" ];
+      script =
+        "exec ${pkgs.clash}/bin/clash -d /etc/clash";
+      unitConfig = {
+        ConditionPathExists = "/etc/clash/config.yaml";
+      };
+      serviceConfig = {
+        ExecStartPre = "+${preStartScript}";
+        ExecStopPost = "+${postStopScript}";
+        AmbientCapabilities =
+          "CAP_NET_BIND_SERVICE CAP_NET_ADMIN";
+        User = "clash";
+        Restart = "on-abort";
+      };
+    };
+    users.users.clash = {
+      isSystemUser = true;
+    };
+
   # FIXME: https://github.com/NixOS/nixpkgs/issues/97389
   # services.syncthing = {
   #   enable = true;
