54569d49830794a73efa8985eb14627d520905d6
diffumist <git@diffumist.me>
2021-11-07 15:11:46 +0800

    delete node amd nft init

--- hosts/vessel/boot.nix
+++ hosts/vessel/boot.nix
@@ -4,7 +4,7 @@
   boot.initrd.kernelModules = [ "nvme" ];
 
   boot.kernel.sysctl = {
-    "vm.swappiness" = 30;
+    "vm.swappiness" = 10;
     "net.core.default_qdisc" = "fq";
     "net.ipv4.tcp_congestion_control" = "bbr";
     "net.ipv4.ip_forward" = 1;
@@ -25,13 +25,12 @@
         options = [ "noatime" "compress-force=zstd" "space_cache=v2" ] ++ options;
         neededForBoot = true;
       };
-      tmpfs = {
+    in
+    {
+      "/" = {
         fsType = "tmpfs";
         options = [ "defaults" "mode=755" ];
       };
-    in
-    {
-      "/" = tmpfs;
       "/.subvols" = btrfs [ ];
       "/boot" = btrfs [ "subvol=boot" ];
       "/nix" = btrfs [ "subvol=nix" ];
