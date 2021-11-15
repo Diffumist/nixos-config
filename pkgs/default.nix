565972feb4d12dbbd181d2f1dda825e3bb50b97c
Diffumist <git@diffumist.me>
2021-11-15 10:49:22 +0800

    ci: update actions

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -8,7 +8,7 @@ rec {
         value = f name;
       })
       (filter (v: v != null) (attrValues
-        (mapAttrs (k: v: if v == "directory" then k else null)
+        (mapAttrs (k: v: if v == "directory" && k != "_sources" then k else null)
           (readDir ./.)))));
   packages = pkgs: mapPackages (name: pkgs."${name}");
   overlay = final: prev:
