7763cb32a60dbeb419c847da1d38420f06dc1870
Diffumist <git@diffumist.me>
2022-01-03 14:25:11 +0800

    clear udev rule

--- hosts/local/software.nix
+++ hosts/local/software.nix
@@ -28,13 +28,6 @@
   services.udev = {
     packages = [ pkgs.libu2f-host ];
     extraRules = ''
-      SUBSYSTEM!="usb", GOTO="canokeys_rules_end"
-      ACTION!="add|change", GOTO="canokeys_rules_end"
-      ATTRS{idVendor}=="20a0", ATTRS{idProduct}=="42d4", ENV{ID_SMARTCARD_READER}="1"
-      LABEL="canokeys_rules_end"
-
-      KERNEL=="hidraw*", SUBSYSTEM=="hidraw", ATTRS{idVendor}=="20a0", ATTRS{idProduct}=="42d4", TAG+="uaccess", GROUP="plugdev", MODE="0660"
-
       SUBSYSTEMS=="usb", ATTRS{idVendor}=="20a0", ATTRS{idProduct}=="42d4", MODE="0666"
     '';
   };
